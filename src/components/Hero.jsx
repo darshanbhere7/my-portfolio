84b471ca88d069d784269398788fe5d160318f2f
Darshan Bhere <darshanbhere2@gmail.com>
2025-07-11 22:31:35 +0530

    Final changes for deployment5

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-scroll';
 const Hero = () => {
   const handleDownloadResume = () => {
     // Convert Google Drive share link to direct download link
-    const fileId = '143BOJulR9zM_lAw3oUADVUgwExNVTJSB';
+    const fileId = '1MhqKG7W99qBXDK7M5tGpDSIbH0DQg18j';
     const downloadUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
     
     // Create a temporary link and trigger download
